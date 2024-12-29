b7b1777cc4258c5f14bdf8d115b0a87a12eb8ccf
juan arango <juanarangozapata35@gmail.com>
2024-12-29 18:31:08 -0500

    agrega la fn crearpaciente desde es store

--- src/components/PacientesList.tsx
+++ src/components/PacientesList.tsx
@@ -1,6 +1,8 @@
-
+import { usePacienteStore } from "../store/store"
 
 const PacientesList = () => {
+  const pacientes = usePacienteStore((state) => state.pacientes)
+  console.log(pacientes)
   return (
     <div>
         Lista del paciente
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,25 @@
 import { create } from "zustand";
 import { DraftPatient, Paciente } from "../types";
+import { v4 as uuidv4 } from 'uuid';
 
 
 //este es el tipado de los elementos que estaran en el store
 type PatienteState = {
-    paciente : Paciente[]
+    pacientes : Paciente[]
     agregaPaciente: (data:DraftPatient) => void
 }
 
+const createPaciente= (paciente:DraftPatient):Paciente => {
+    return  {...paciente, id: uuidv4()}
+}
 
 //estas son las funciones que se obtendran del store, para usarlas en los componentes
-export const usePacienteStore = create<PatienteState>(()=>({
-    paciente: [],
-    agregaPaciente: (data:DraftPatient) => {
-        console.log(data)
+export const usePacienteStore = create<PatienteState>((set)=>({
+    pacientes: [],
+    agregaPaciente: (data) => {
+        const nuevoPaciente = createPaciente(data)
+        set((state)=>({
+            pacientes: [...state.pacientes, nuevoPaciente]
+        }))
     }
 }))
