cfdfafc3cfc9f011679df2653e781ea1ac858955
juan arango <juanarangozapata35@gmail.com>
2024-12-29 22:01:07 -0500

    agrega funcionalidad de eliminar paciente

--- src/components/DetallePaciente.tsx
+++ src/components/DetallePaciente.tsx
@@ -1,3 +1,4 @@
+import { usePacienteStore } from "../store/store"
 import { Paciente } from "../types"
 import DetallePacienteItem from "./DetallePacienteItem"
 
@@ -5,6 +6,7 @@ type DetallePacienteProps = {
     paciente: Paciente
 }
 const DetallePaciente = ({paciente}: DetallePacienteProps) => {
+    const eliminarPaciente = usePacienteStore(state => state.eliminarPaciente)
   return (
     <div className="mx-5 my-10 px-5 py-10 bg:white shadow-md rounded-xl">
       <DetallePacienteItem  label="ID"  data={paciente.id}/>
@@ -15,7 +17,7 @@ const DetallePaciente = ({paciente}: DetallePacienteProps) => {
       <DetallePacienteItem  label="Sintomas"  data={paciente.symptoms}/>
       <div className="flex justify-between  mt-10">
         <button className="py-2 px-3 text-white bg-indigo-600 hover:bg-indigo-800 font-bold uppercase rounded-lg">Editar</button>
-        <button className="py-2 px-3 text-white bg-red-600 hover:bg-red-800 font-bold uppercase rounded-lg">Eliminar</button>
+        <button className="py-2 px-3 text-white bg-red-600 hover:bg-red-800 font-bold uppercase rounded-lg" onClick={()=> eliminarPaciente(paciente.id)}>Eliminar</button>
 
       </div>
     </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,8 +5,10 @@ import { v4 as uuidv4 } from 'uuid';
 
 //este es el tipado de los elementos que estaran en el store
 type PatienteState = {
+    //la funciones que voy a utlizar en los componentes siempre deben estar en el tipado
     pacientes : Paciente[]
     agregaPaciente: (data:DraftPatient) => void
+    eliminarPaciente: (id:Paciente['id']) => void 
 }
 
 const createPaciente= (paciente:DraftPatient):Paciente => {
@@ -21,5 +23,11 @@ export const usePacienteStore = create<PatienteState>((set)=>({
         set((state)=>({
             pacientes: [...state.pacientes, nuevoPaciente]
         }))
+    },
+    eliminarPaciente:(id)=> {
+        set((state)=>({
+            pacientes: state.pacientes.filter(paciente => paciente.id !== id)
+        }))
     }
+
 }))
